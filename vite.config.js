11db12d2be16f9f5b301c1413847aafd11ff5cf3
Rando Õispuu <rando@innofly.eu>
2021-03-23 15:32:33 +0200

    Fix conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import vue from "@vitejs/plugin-vue";
+import liveReload from "vite-plugin-live-reload";
 /**
  * @type {import('vite').UserConfig}
  */
@@ -6,9 +7,19 @@ export default {
     publicDir: "disable",
     plugins: [
         vue(),
+        liveReload(
+            "(app|config|database|public|resources|routes|tests)/**/*.php"
+        ),
     ],
     build: {
         manifest: true,
         outDir: "public/dist",
-    }
+    },
+    resolve: {
+        alias: {
+            "@inertiajs/inertia-vue3":
+                "@inertiajs/inertia-vue3/dist/index.umd.js",
+            "@inertiajs/inertia": "@inertiajs/inertia/dist/index.umd.js",
+        },
+    },
 };
