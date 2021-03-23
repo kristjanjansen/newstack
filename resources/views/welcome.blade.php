a0bc9cf146187d17cde6da770ce1b49dcc0bf805
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-23 10:37:13 +0200

    feat: build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 
 <head>
     <meta charset="utf-8">
@@ -11,11 +11,7 @@
 
 <body>
     @production
-    @php
-    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-    @endphp
-    <script type="module" src="/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>
-    <link rel="stylesheet" href="/build/{{ $manifest['resources/js/app.js']['css'][0] }}">
+    @vite
     @else
     <script type="module" src="http://localhost:3000/@@vite/client"></script>
     <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
