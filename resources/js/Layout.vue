06c960ef9718fc30f88d58ae91a0789e97134cac
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-24 09:24:07 +0100

    rollback

--- resources/js/Layout.vue
+++ resources/js/Layout.vue
@@ -1,7 +1,6 @@
 <template>
     <main>
         <div class="p-4">
-            Second layout: &nbsp;
             <inertia-link href="/">First</inertia-link>
             &nbsp;
             <inertia-link href="/second">Second</inertia-link>
