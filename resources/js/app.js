e1c77add8ea3c1b3c1b2d554616d5165a6451e20
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-25 19:33:31 +0200

    cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,12 @@
 import "vite/dynamic-import-polyfill";
 
-import { createApp, h } from "vue";
+import { createApp, h, defineAsyncComponent } from "vue";
 import { App, plugin } from "@inertiajs/inertia-vue3";
 
 import "../css/app.css";
 
 const pages = import.meta.glob("./Pages/**/*.vue");
 const layouts = import.meta.globEager("./Layouts/**/*.vue");
-console.log(layouts);
 
 const el = document.getElementById("app");
 
@@ -16,14 +15,12 @@ createApp({
         h(App, {
             initialPage: JSON.parse(el.dataset.page),
             resolveComponent: (name) => {
-                const importPage = pages[`./Pages/${name}.vue`];
-                return importPage().then(({ default: page }) => {
-                    page.layout =
-                        layouts[
-                            `./Layouts/${page.layoutName || "Yellow"}.vue`
-                        ].default;
-                    return page;
-                });
+                const page = defineAsyncComponent(pages[`./Pages/${name}.vue`]);
+                page.layout =
+                    layouts[
+                        `./Layouts/${page.layoutName || "Yellow"}.vue`
+                    ].default;
+                return page;
             },
         }),
 })
